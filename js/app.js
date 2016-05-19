d60752424072d01f85e159b82074b55fc01420bf
Adrian J Lopez <adrianjaylopez@gmail.com>
2016-05-19 17:04:05 -0400

    Created .hide class in style.css. Added hide to quiz, status, and question in index.html. Added removeclass to hide for quiz, status, and question, added hide to introduction. Added musicimage to musicplaceholder in app.js.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
-  $('#startGame').submit();
+  $('#startGame').submit(function(e) {
+    $('.status').removeClass('hide');
+    $('.question').removeClass('hide');
+    $('.quiz').removeClass('hide');
+    $('.introduction').addClass('hide');
+    $('.musicPlaceholder').addClass('musicImage');
+    $('.titleScreen h1').addClass('titleAnimation');
+
+    e.preventDefault();
+  });
   $('#answerForm').submit();
 });
 function newGame() {}
