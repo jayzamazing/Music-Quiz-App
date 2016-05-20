c96ef05a8d4698d16b06c3a1366986035aa1cc89
Adrian J Lopez <adrianjaylopez@gmail.com>
2016-05-20 12:30:46 -0400

    Moved initialization of currentgame into startgame submit function, created songscallback function and moved quiz content selector into this function. Added variable to getsonginfo to use as callback and added complete to know when json data retrieval is successful. Added minor documentation to app.js.

--- js/app.js
+++ js/app.js
@@ -1,30 +1,54 @@
 $(document).ready(function() {
-  var currentGame = newGame();
-  currentGame.getSongInfo();
-  var data;
+  var currentGame;
+  /*
+  * game initialization once start game button is pressed
+  * hide initial items and show game items
+  */
   $('#startGame').submit(function(e) {
+    /*  hide introduction and show game items */
     $('.status').removeClass('hide');
     $('.question').removeClass('hide');
     $('.quiz').removeClass('hide');
     $('.introduction').addClass('hide');
     $('.musicPlaceholder').addClass('musicImage');
     $('.titleScreen h1').addClass('titleAnimation');
-    $('.quiz .content').html(currentGame.songs.songDetails[0].lyrics);
+    /* initialize game  */
+    currentGame = new newGame();
+    /* get list of songs and pass in call back */
+    currentGame.getSongInfo(songsCallBack);
     e.preventDefault();
   });
   $('#answerForm').submit();
+  /*
+  * Callback function for when json request comes back successfully
+  */
+  function songsCallBack() {
+    $('.quiz .content').html(currentGame.songs.songDetails[0].lyrics);
+  }
 });
+/*
+* Initialize game and set game state
+*/
 function newGame() {
   return new Game();
 }
+/*
+* Game object to store state
+*/
 function Game() {
   var currentQuestion = 0;
   var answeredCorrectly = 0;
+  /* object to hold json query of songs */
   this.songs = [];
 }
-Game.prototype.getSongInfo = function() {
+/*
+* Function to get song data from json
+*/
+Game.prototype.getSongInfo = function(callback) {
   var context = this;
   $.getJSON("data/songs.json", "context", function(data) {
     context.songs = data;
+  }).complete(function() {
+    callback();
   });
 };
