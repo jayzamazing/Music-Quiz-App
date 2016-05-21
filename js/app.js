092f0209b553ee4c944e90a447e7b2e6db3fe2aa
Adrian J Lopez <adrianjaylopez@gmail.com>
2016-05-20 21:40:33 -0400

    Added answerform submit section, added get/set status to game, added next function to app.js. Fixed issue with i tags, added values to radio inputs, added hide class to next div in index.html.

--- js/app.js
+++ js/app.js
@@ -16,22 +16,53 @@ $(document).ready(function() {
     currentGame = new newGame(songsCallBack);
     e.preventDefault();
   });
-  $('#answerForm').submit();
+  /*
+  * Function to set the status bar, green is passed, red otherwise
+  */
+  $('#answerForm').submit(function(e) {
+    currentGame.setCurrentQuestion();
+    var music = currentGame.getStatus() + 1;
+    var arrow = music + 1;
+    //if item checked matches the correct answer for the question
+    if ($("input[name=radios]:checked").val() == currentGame.correctAnswer) {
+      $('.status li:nth-child(' + music + ')').find('i').css('color', 'green');
+    } else {
+      $('.status li:nth-child(' + music + ')').find('i').css('color', 'red');
+    }
+    $('.status li:nth-child(' + arrow + ')').find('i').css('color', 'blue');
+    //disable the submit
+    $('#button6').addClass('hide');
+    //hide next if there are no more questions
+    if (currentGame.getCurrentQuestion() != 5) {
+      $('.next').removeClass('hide');
+    }
+    currentGame.setStatus();
+    e.preventDefault();
+  });
+  /*
+  * Function to load the next set of lyrics and music artists
+  */
+  $('.next').click(function() {
+    //show the items on the screen
+    setAnswers();
+    $('.next').addClass('hide');
+    $('#button6').removeClass('hide');
+  });
   /*
   * Callback function for when json request comes back successfully
   */
   function songsCallBack() {
-    $('.quiz .content').html(currentGame.songs.songDetails[currentGame.getCurrentQuestion()].lyrics);
+    $('.quiz .content').html(currentGame.songs.songDetails[currentGame.getCurrentQuestion() + 1].lyrics);
     setAnswers();
   }
   function setAnswers() {
     for (i = 1; i <= 5; i++) {
       if (i === currentGame.correctAnswer) {
         $('#radio' + i).html(currentGame.songs.songDetails[currentGame.getCurrentQuestion()].songName  + '<br>' +
-          currentGame.songs.songDetails[currentGame.getCurrentQuestion()].songName);
+          currentGame.songs.songDetails[currentGame.getCurrentQuestion()].songArtist);
       } else {
-        $('#radio' + i).html(currentGame.songs.otherSongDetails[i].songName + '<br>' +
-          currentGame.songs.otherSongDetails[i].songArtist);
+        $('#radio' + i).html(currentGame.songs.otherSongDetails[currentGame.getcurrentOtherQuestion()].songName + '<br>' +
+          currentGame.songs.otherSongDetails[currentGame.getcurrentOtherQuestion()].songArtist);
           /* increment count on bad answers */
           currentGame.setcurrentOtherQuestion();
       }
@@ -56,10 +87,19 @@ function Game() {
   var currentQuestion = 0;
   var currentOtherQuestion = 0;
   var answeredCorrectly = 0;
+  var status = 0;
   /* object to hold json query of songs */
   this.songs = [];
   /* Number that has the correct answer for current question */
   this.correctAnswer = 0;
+  /* get status number */
+  this.getStatus = function() {
+    return status;
+  };
+  /* set status + 2 (elements controlled) */
+  this.setStatus = function() {
+    status += 2;
+  };
   /* get currentQuestion */
   this.getCurrentQuestion = function() {
     return currentQuestion;
