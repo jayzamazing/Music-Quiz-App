f48cd69b413ccaec12fc4226872c0d75f6691c0a
Adrian J Lopez <adrianjaylopez@gmail.com>
2016-06-03 18:10:21 -0400

    Added logic to getcategories and getplaylist functions to filter the json out and map it into a format i can use.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var app = express();
 var req = require('request');
-var spotifyToken, categories, playList;
+var spotifyToken, categories = [], playList = [];
 app.use('static', express.static(__dirname));//set the base directory to find resources
 /*
 * Function that gets initialized when navigating to base url, sends back index.html
@@ -38,9 +38,9 @@ function getToken() {
   //post to spotify requesting token
   req.post(authOptions, function(error, response, body) {
   if (!error && response.statusCode === 200) {//if status 200
-    //spotifyToken = body.access_token;//store token
+    spotifyToken = body.access_token;//store token
     //getCategories(spotifyToken, 'pop');
-    //getPlayList(spotifyToken, 'spotify', '5FJXhjdILmRA2z5bvz4nzf');
+    getPlayList(spotifyToken, 'spotify', '5FJXhjdILmRA2z5bvz4nzf');
   }
   });
 }
@@ -53,8 +53,17 @@ function getCategories(spotifyToken, category) {
   };
   req.get(searchInfo, function(error, response, body) {
     if (!error && response.statusCode === 200) {//if status 200
-      categories = body;
-      console.log(categories);
+      categories = JSON.parse(body);
+      categories = categories.playlists.items.filter(function(item) {
+        if (item.tracks.total > 50)//minimum number of songs needed for quiz with space for null values
+          return item;
+      })
+      .map(function(obj) {
+          var temp = {};
+          temp[obj.name] = obj.id;
+          return temp;
+        });
+        console.log(categories);
     }
   });
 }
@@ -67,7 +76,19 @@ function getPlayList(spotifyToken, user, playListId) {
   };
   req.get(searchInfo, function(error, response, body) {
     if (!error && response.statusCode === 200) {//if status 200
-       playList = body;
+       playList = JSON.parse(body);
+       playList = playList.tracks.items.filter(function(item) {
+         //ensure all values are not null
+         if (item.track.name && item.track.preview_url && item.track.artists[0].name && item.track.album.images[2].url){
+           return item;
+         }
+       })
+       .map(function(item) {
+         var temp = {};
+         temp[item.track.name] = [item.track.preview_url,
+           item.track.artists[0].name, item.track.album.images[2].url];
+         return temp;
+       });
        console.log(playList);
     }
   });
