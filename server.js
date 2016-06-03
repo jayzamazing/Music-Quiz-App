aeffb1d80eaab864b78e5d851af1c5a18f4fb0e6
Adrian J Lopez <adrianjaylopez@gmail.com>
2016-06-03 10:56:00 -0400

    Moved spotify get token logic into its own function, moved auth options into gettoken function in server.js.

--- server.js
+++ server.js
@@ -9,32 +9,36 @@ app.use('static', express.static(__dirname));//set the base directory to find re
 * and gets authentication token from spotify
 */
 app.get('/', function (request, response) {
-  req.post(authOptions, function(error, response, body) {
-  if (!error && response.statusCode === 200) {
-    spotifyToken = body.access_token;
-    console.log(spotifyToken);
-    }
-});
+  getToken();
   response.sendFile(__dirname + '/index.html');
 });
-
-
-/*
-* Authentication options used by spotify api for client credentials work flow
-*/
-var authOptions = {
-  url: 'https://accounts.spotify.com/api/token',
-  headers: {
-    'Authorization': 'Basic ' + (new Buffer(process.env.client_id + ':' + process.env.client_secret).toString('base64'))
-  },
-  form: {
-    grant_type: 'client_credentials'
-  },
-  json: true
-};
 /*
 * Start up node and listen on a specified port
 */
 listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+/*
+* Function to get client credentials token from spotify
+*/
+function getToken() {
+  /*
+  * Authentication options used by spotify api for client credentials work flow
+  */
+  var authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: {
+      'Authorization': 'Basic ' + (new Buffer(process.env.client_id + ':' + process.env.client_secret).toString('base64'))
+    },
+    form: {
+      grant_type: 'client_credentials'
+    },
+    json: true
+  };
+  //post to spotify requesting token
+  req.post(authOptions, function(error, response, body) {
+  if (!error && response.statusCode === 200) {//if status 200
+    spotifyToken = body.access_token;
+  }
+  });
+}
